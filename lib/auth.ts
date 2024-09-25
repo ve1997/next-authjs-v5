7d1b1455f7ed17c2ef055604fdea7e97f7606434
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-25 16:21:25 +0900

    test: fix auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,22 +6,29 @@ const config: NextAuthConfig = {
 	// 	logo: "https://next-auth.js.org/img/logo/logo-sm.png",
 	// },
 	providers: [Google],
-	// basePath: "/api/auth",
-	// callbacks: {
-	// 	authorized({ request, auth }) {
-	// 		try {
-	// 			const { pathname } = request.nextUrl;
-	// 			if (pathname === "/protected-page") return !!auth;
-	// 			return true;
-	// 		} catch (e) {
-	// 			console.error(e);
-	// 		}
-	// 	},
-	// 	jwt({ token, trigger, session }) {
-	// 		if (trigger === "update") token.name = session.user.name;
-	// 		return token;
-	// 	},
+	// pages: {
+	// 	signIn: "/auth/signin",
 	// },
+	// basePath: "/api/auth",
+	callbacks: {
+		authorized({
+			//  request,
+			auth,
+		}) {
+			try {
+				// const { pathname } = request.nextUrl;
+				// if (pathname === "/server-example") return !!auth;
+				// return true;
+				return !!auth;
+			} catch (e) {
+				console.error(e);
+			}
+		},
+		jwt({ token, trigger, session }) {
+			if (trigger === "update") token.name = session.user.name;
+			return token;
+		},
+	},
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
